d83a5e1eb79f05ad5dd3fae77147210c102bdc64
ellian-aragao <ellian.aragao@gmail.com>
2019-09-22 12:54:46 -0300

    alterando pasta e add comentários

--- calculoNumerico/python/taylor.py
+++ calculoNumerico/python/taylor.py
@@ -2,18 +2,13 @@ def FdeX(x):  # função de x
     return (- 0.1 * x ** 4 - 0.15 * x ** 3 - 0.5 * x ** 2 - 0.25 * x + 1.2)
 
 
-def fat(n):  # função fatorial
-    if (n == 0):
-        return 1
-    else:
-        return (n * fat(n-1))
-
-
 def Taylor(coeficientes, x, i):  # função de Taylor
-    return ((fat(i) * coeficientes) * pow(x, i) / fat(i))
+    return ( coeficientes * pow(x, i))
 
 
+# coeficientes dados pela lista
 coeficientes = [- 0.1, - 0.15, - 0.5, - 0.25, 1.2]
+# intervalo dado [0,4] que foi dividido em dez partes
 x = [0, 0.4, 0.8, 1.2, 1.6, 2.0, 2.4, 2.8, 3.2, 3.6, 4]
 coeficientes.reverse()
 
