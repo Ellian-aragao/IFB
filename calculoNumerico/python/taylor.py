b1712c10d369ed73364397aaca8ad146e835db04
ellian-aragao <ellian.aragao@gmail.com>
2019-09-22 19:15:41 -0300

    corrigindo erro para encotnra a função

--- calculoNumerico/python/taylor.py
+++ calculoNumerico/python/taylor.py
@@ -1,20 +1,33 @@
+import numpy
+
+
 def FdeX(x):  # função de x
     return (- 0.1 * x ** 4 - 0.15 * x ** 3 - 0.5 * x ** 2 - 0.25 * x + 1.2)
 
 
 def Taylor(coeficientes, x, i):  # função de Taylor
-    return ( coeficientes * pow(x, i))
+    return (coeficientes * x ** i)
 
 
 # coeficientes dados pela lista
 coeficientes = [- 0.1, - 0.15, - 0.5, - 0.25, 1.2]
+
 # intervalo dado [0,4] que foi dividido em dez partes
 x = [0, 0.4, 0.8, 1.2, 1.6, 2.0, 2.4, 2.8, 3.2, 3.6, 4]
+
+# para a função de Taylor fazer o cálculo com os valores corretos
 coeficientes.reverse()
 
+# alterando os valores de x
 for i in x:
-    print('quando x = {}'.format(i))
     taylor = 0
-    for j in range(0, 4):
-        taylor = Taylor(coeficientes[j], i, j)
-        print(taylor, FdeX(i))
+    fdex = FdeX(i)
+    # realizando as iterações de taylor para chegar no valor de f(x)
+    for j in range(len(coeficientes)):
+        taylor += Taylor(coeficientes[j], i, j)
+        print(j, i, taylor, fdex)
+
+        # critério de parada quando elementos são iguais
+        if abs(taylor - fdex) < 1e-3:
+            break
+    print('\n\n')
