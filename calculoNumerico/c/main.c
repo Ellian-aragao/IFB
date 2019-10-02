ca29daf2dd8f088336daba265836444196b55cd5
ellian-aragao <ellian.aragao@gmail.com>
2019-10-02 00:32:02 -0300

    transformando os algoritmos em um unico programa para simplicidade

---
+++ calculoNumerico/c/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "funcoes.h"
+#define TRUE 1
+#define FALSE 0
+int main()
+{
+    int parametro = TRUE;
+    limpartela();
+    while (parametro)
+    {
+        puts("Digite qual opção deseja testar");
+        puts("1 - epsilon da maquina");
+        puts("2 - Taylor");
+        puts("3 - bisseccao");
+        puts("0 - sair");
+        putchar('\n');
+        int opcao;
+        scanf("%d",&opcao);
+        switch (opcao)
+        {
+            case 1:
+                epsilon();
+                break;
+            
+            case 2:
+                taylor();
+                break;
+            
+            case 3:
+                bisseccao();
+                break;
+            
+            case 0:
+                parametro = FALSE;
+                break;
+
+            default:
+                printf("opcao invalida\n\n");
+                break;
+        }
+    }
+    return 0;
+}
