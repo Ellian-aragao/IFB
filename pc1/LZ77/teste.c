e86dd0b35de49a753ddb766bb7fd365ffdd211a5
ellian-aragao <ellian.aragao@gmail.com>
2019-10-15 00:02:44 -0300

    tentando corrigir erro de cópia da string

--- pc1/LZ77/teste.c
+++ pc1/LZ77/teste.c
@@ -12,11 +12,9 @@ void concatenacao(char *destino, int tamanho_concat, char *str_concat)
 }
 void concatenacao_lim(char *destino, int tamanho_concat, char *str_concat, int limite_concat)
 {
-    int i, j;
-    for (i = tamanho_concat, j = 0; j < limite_concat ; i++, j++)
-    {
-        destino[i] = str_concat[j];
-    }
+    int i;
+    for (i = tamanho_concat; i;)
+        ;
 }
 
 int main()
@@ -28,7 +26,7 @@ int main()
     substring = strtok(str, "(,)"); // operação de iniciação de leitura da string
 
     char str_final[100]; // string decodificada
-    int str_tam = 0;     // tamanho do str_final
+    int str_tam = 0;     // tamanho da str_final
 
     for (int i = 1; substring != NULL; i++)
     {
@@ -38,53 +36,56 @@ int main()
 
         if (i % 3 == 0 && i <= 3) // condição para iniciação da string com o primeiro pack da codificação
         {
-            // puts("primeira letra");
+            // printf("3º parametro = %s\n", substring);
             strcat(str_final, substring); // add da primeira letra na string final
             str_tam++;                    // "strlen" manual
         }
         else if (i % 3 == 1)
         {
-            // puts("primeiro parametro");
             pri_parametro = atoi(substring); // salvar o número da codificação
+            // printf("1º parametro = %d\n", pri_parametro);
         }
         else if (i % 3 == 2)
         {
-            // puts("segundo parametro");
             seg_parametro = atoi(substring); // salvar o número da codificação
+            // printf("2º parametro = %d\n", seg_parametro);
         }
         else if (i % 3 == 0 && i > 3)
         {
-            // puts("letra");
-
 
             if (pri_parametro > 0)
             {
-                char *ptr_str = malloc(pri_parametro * sizeof(char));   // armazena o tamanho do parametro 'x'
-                int diferenca = str_tam - pri_parametro;                // ponto de inicio para cópia
+                char *ptr_str = malloc((pri_parametro + 1) * sizeof(char)); // armazena o tamanho do parametro 'x'
+                int diferenca = str_tam - pri_parametro;              // ponto de inicio para cópia
 
-
-                for (int j = 0; j < pri_parametro; j++, diferenca++)    // realiza a cópia dos caracteres do parametro 'x
+                for (int j = 0; j <= pri_parametro; j++, diferenca++) // realiza a cópia dos caracteres do parametro 'x'
                 {
                     ptr_str[j] = str_final[diferenca];
                 }
-                printf("copia ->%s\n",ptr_str);
+                printf("copia ->%s\n", ptr_str);
 
-
-                if ()
+                while (seg_parametro - pri_parametro >= 0)
                 {
-                    strcat
+                    strcat(str_final, ptr_str);
+                    str_tam += seg_parametro;
+                    seg_parametro -= pri_parametro;
                 }
+
                 for (int j = 0; j < seg_parametro; j++)
                 {
-                    for (int t = 0; t < pri_parametro - 1; t++)
-                    {
-                        str_tam++;
-                        str_final[str_tam] = ptr_str[t];
-                        //concatenacao_lim(str_final, str_tam, ptr_str,seg_parametro);
-                    }
+                    str_final[str_tam] = ptr_str[j];
                 }
+
+                // for (int j = 0; j < seg_parametro; j++)
+                // {
+                //     for (int t = 0; t < pri_parametro - 1; t++)
+                //     {
+                //         str_tam++;
+                //         str_final[str_tam] = ptr_str[t];
+                //         //concatenacao_lim(str_final, str_tam, ptr_str,seg_parametro);
+                //     }
+                // }
             }
-            
 
             /*                              copia através do ponteiro que percorre a str_final                 */
             // if (seg_parametro > 0)
@@ -105,17 +106,16 @@ int main()
             //     }
             // }
 
-
+            printf("3º parametro = %s\n", substring);
             strcat(str_final, substring);
             str_tam++;
+            puts(str_final);
         }
-
-        printf("%s\n", substring);
         substring = strtok(NULL, "(,)"); // atualização do ponteiro com a substring atual
     }
 
     putchar('\n');
-    strcat(str_final, "\0");
+    //strcat(str_final, "\0");
     puts(str_final);
     puts("ababcbababaaaaaa\\0");
 
