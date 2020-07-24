c522027e366e093c2ecb56dd82cc3280d79fda21
ellian-aragao <ellian.aragao@gmail.com>
2020-07-24 20:48:21 -0300

    correção e refatoração

--- ed1/listas/estatica/staticlist.h
+++ ed1/listas/estatica/staticlist.h
@@ -5,14 +5,18 @@ typedef struct aluno{
     int matricula;
     char nome[20];
     float n1,n2,n3;
-}aluno;
-turma* turma_cria();
-void turma_libera(turma* t1);
-int turma_tamanho(turma* t1);
-int turma_cheia(turma* t1);
-int turma_vazia(turma* t1);
-int turma_insere_ordenado(turma* t1, aluno a1);
-int turma_remove_ordenado(turma* t1, aluno a1);
-int turma_consulta_posicao(turma* t1, int posicao, aluno* a1);
-int turma_consulta_busca_matricula(turma* t1,int matricula, aluno* a1);
+}ALUNO;
+typedef struct turma{
+    int quantidade;
+    ALUNO alunos[TAM];
+}TURMA;
+TURMA* turma_cria();
+void turma_libera(TURMA* t1);
+int turma_tamanho(TURMA* t1);
+int turma_cheia(TURMA* t1);
+int turma_vazia(TURMA* t1);
+int turma_insere_ordenado(TURMA* t1, ALUNO a1);
+int turma_remove_ordenado(TURMA* t1, ALUNO a1);
+int turma_consulta_posicao(TURMA* t1, int posicao, ALUNO* a1);
+int turma_consulta_busca_matricula(TURMA* t1,int matricula, ALUNO* a1);
 #endif
