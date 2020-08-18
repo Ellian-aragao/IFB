a158681867308183d5dc5fd9954054e22c4ffb74
ellian_aragao <ellian.aragao@gmail.com>
2020-08-18 14:22:15 -0300

    erro na intersseccao

--- ed1/1 - TAD/5/conjuntoInt.c
+++ ed1/1 - TAD/5/conjuntoInt.c
@@ -69,31 +69,109 @@ CONJUNTO *insere(CONJUNTO *conjunto, long valorAdicionar)
   return conjunto;
 }
 
-// CONJUNTO *uniao() {}
-// CONJUNTO* remove() {}
-// CONJUNTO* interseccao() {}
+CONJUNTO *finalizaCopiaParaUniao(CONJUNTO *conjuntoUniao, CONJUNTO *aFinalizar, unsigned long indice)
+{
+  while (indice < aFinalizar->tam)
+    conjuntoUniao = insere(conjuntoUniao, aFinalizar->vetor[indice++]);
+  return conjuntoUniao;
+}
+
+void printConjunto(CONJUNTO *conj)
+{
+  for (unsigned long i = 0; i < conj->tam; i++)
+  {
+    printf("%ld,", conj->vetor[i]);
+  }
+}
+
+CONJUNTO *uniao(CONJUNTO *conj1, CONJUNTO *conj2)
+{
+  CONJUNTO *uniaoConjuntos = criaConjuntoVazio();
+  unsigned long i, tam = conj1->tam < conj2->tam ? conj1->tam : conj2->tam;
+  for (i = 0; i < tam; i++)
+  {
+    uniaoConjuntos = insere(uniaoConjuntos, conj1->vetor[i]);
+    uniaoConjuntos = insere(uniaoConjuntos, conj2->vetor[i]);
+  }
+  uniaoConjuntos = (conj1->tam > conj2->tam ? finalizaCopiaParaUniao(uniaoConjuntos, conj1, i) : finalizaCopiaParaUniao(uniaoConjuntos, conj2, i));
+  return uniaoConjuntos;
+}
+
+CONJUNTO *removeItemConjunto(CONJUNTO *conj, long itemRetirar)
+{
+  unsigned long index;
+  if (testaSePertence(conj, itemRetirar, &index))
+  {
+    while (index < conj->tam)
+    {
+      conj->vetor[index] = conj->vetor[index + 1];
+      index++;
+    }
+    long *tmp = realloc(conj->vetor, (--conj->tam) * sizeof(long));
+    if (tmp == NULL)
+    {
+      fprintf(stderr, "Erro na remoção do item '%ld' no conjunto\n", itemRetirar);
+      return NULL;
+    }
+    conj->vetor = tmp;
+    return conj;
+  }
+  return NULL;
+}
+
+CONJUNTO *interseccao(CONJUNTO *conj1, CONJUNTO *conj2)
+{
+  CONJUNTO *interseccaoConjuntos = criaConjuntoVazio();
+
+  for (unsigned long i = 0; i < conj1->tam; i++)
+  {
+    if (testaSePertence(conj2, conj1->vetor[i], NULL))
+      insere(interseccaoConjuntos, conj1->vetor[i]);
+  }
+
+  if (interseccaoConjuntos->tam == 0)
+  {
+    perror("Não existe interseccao entre os conjuntos\n");
+    liberaConjunto(interseccaoConjuntos);
+    return NULL;
+  }
+  return interseccaoConjuntos;
+}
+
 // CONJUNTO* diferenca() {}
 
 // tentativa de algoritmo bisseccao baseado em calculo numérico 2ºsem 2019
-int testaSePertence(CONJUNTO *conjunto, long numeroProcurado)
+int testaSePertence(CONJUNTO *conjunto, long numeroProcurado, unsigned long *index)
 {
-  long *vetor = conjunto->vetor;
   unsigned long
       limiteInferior = 0,
       LimiteSuperior = conjunto->tam,
-      mediana = (long)1 + conjunto->tam / 2;
+      mediana = (unsigned long)1 + conjunto->tam / 2;
+
+  if (conjunto->vetor[0] > numeroProcurado || conjunto->vetor[conjunto->tam] < numeroProcurado)
+    return FALSE;
 
   while (TRUE)
   {
-    if (vetor[mediana] > numeroProcurado)
+    printf("%ld %ld %ld --> %ld\n", limiteInferior, mediana, LimiteSuperior, numeroProcurado);
+    getchar();
+    if (numeroProcurado < conjunto->vetor[mediana])
+    {
+      puts("encontrado");
       LimiteSuperior = mediana;
-    else if (vetor[mediana] < numeroProcurado)
+    }
+    else if (conjunto->vetor[mediana] < numeroProcurado)
+    {
       limiteInferior = mediana;
-    else if (vetor[mediana] == numeroProcurado)
+    }
+    else if (conjunto->vetor[mediana] == numeroProcurado)
+    {
+      *index = mediana;
       return TRUE;
-    if ((limiteInferior + 1) == LimiteSuperior)
+    }
+    else if ((limiteInferior + 1) == LimiteSuperior)
       return FALSE;
-    mediana = (long)limiteInferior + (LimiteSuperior - limiteInferior) / 2;
+    mediana = (unsigned long)limiteInferior + (LimiteSuperior - limiteInferior) / 2;
   }
 }
 
@@ -107,7 +185,8 @@ long maior(CONJUNTO *conjunto)
   return conjunto->vetor[conjunto->tam];
 }
 
-int testaSeOsConjuntosSaoIguais(CONJUNTO* conjunto1, CONJUNTO* conjunto2) {
+int testaSeOsConjuntosSaoIguais(CONJUNTO *conjunto1, CONJUNTO *conjunto2)
+{
   long tam;
   if ((tam = conjunto1->tam) == conjunto2->tam)
   {
@@ -121,26 +200,45 @@ int testaSeOsConjuntosSaoIguais(CONJUNTO* conjunto1, CONJUNTO* conjunto2) {
   return FALSE;
 }
 
-unsigned long tamanho(CONJUNTO* conjunto) {
+unsigned long tamanho(CONJUNTO *conjunto)
+{
   return conjunto->tam;
 }
 
-int testaSeoConjuntoVazio(CONJUNTO* conjunto) {
-  if (conjunto->tam)
+int testaSeoConjuntoVazio(CONJUNTO *conjunto)
+{
+  if (!conjunto->tam)
   {
     return TRUE;
   }
   return FALSE;
 }
 
+void liberaConjunto(CONJUNTO *conj)
+{
+  free(conj->vetor);
+  free(conj);
+}
+
 int main(int argc, char const *argv[])
 {
-  CONJUNTO *ptr = criaConjuntoVazio();
-  for (long i = 1000; i > 0; i--)
+  CONJUNTO *ptr = criaConjuntoVazio(),
+           *ptr2 = criaConjuntoVazio();
+
+  for (long i = 0; i < 5; i++)
   {
     ptr = insere(ptr, i);
+    ptr2 = insere(ptr2, i + 2);
   }
-  printf("%d\n", testaSePertence(ptr, 378));
+
+  printConjunto(ptr);
+  putchar('\n');
+
+  printConjunto(ptr2);
+  putchar('\n');
+
+  printConjunto(interseccao(ptr, ptr2));
+  putchar('\n');
 
   return 0;
 }
--- ed1/1 - TAD/5/conjuntoInt.h
+++ ed1/1 - TAD/5/conjuntoInt.h
@@ -3,4 +3,10 @@
 
 typedef struct conjuntoInt CONJUNTO;
 
+void printConjunto(CONJUNTO *conj);
+int testaSePertence(CONJUNTO *conjunto, long numeroProcurado, unsigned long *index);
+/**
+ * caso encontre, retorna 1, caso não retorna 0
+*/
+void liberaConjunto(CONJUNTO *conj);
 #endif
