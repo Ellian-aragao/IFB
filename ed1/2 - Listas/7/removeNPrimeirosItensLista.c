4bcd3bb1099cac68b26171954a9a9a0783e959c5
ellian_aragao <ellian.aragao@gmail.com>
2020-08-25 18:40:50 -0300

    concluido protótipo para professor, questão 7 ed1

--- ed1/2 - Listas/7/removeNPrimeirosItensLista.c
+++ ed1/2 - Listas/7/removeNPrimeirosItensLista.c
@@ -9,36 +9,89 @@ struct structExemplo
   u_long tam;
 };
 
-int removeNPrimeirosIntensLista(void *vetor, size_t sizeOfElemento, u_long nElementos, u_long maxElementos)
+int removeNPrimeirosIntensLista(void *vetor, size_t sizeOfElement, u_long nElementsToRemove, u_long *addressTamVetor)
 {
-  for (u_long i = nElementos; i > 0; i--)
+  if (nElementsToRemove < *addressTamVetor)
   {
-    void *destino = &(vetor[sizeOfElemento * (i - 1)]),
-         *pontoCopia = &(vetor[sizeOfElemento * i]);
-    memcpy(destino, pontoCopia, sizeOfElemento);
+    u_long i, j;
+    for (i = 0, j = nElementsToRemove; j < *addressTamVetor; i++, j++)
+    {
+      void *elementInitialVector = &(vetor[sizeOfElement * i]),
+           *elementToCopy = &(vetor[sizeOfElement * j]);
+      memmove(elementInitialVector, elementToCopy, sizeOfElement);
+    }
+    *addressTamVetor -= nElementsToRemove;
+    vetor = realloc(vetor, sizeOfElement * (*addressTamVetor));
+    if (vetor != NULL)
+      return 1;
   }
+  else if (nElementsToRemove == *addressTamVetor)
+  {
+    *addressTamVetor = 0;
+    vetor = realloc(vetor, sizeOfElement);
+    if (vetor != NULL)
+      return 1;
+  }
+
   return 0;
+}
+
+ListaDinamica *createListaDinamica()
+{
+  ListaDinamica *lista = malloc(sizeof(ListaDinamica));
+  lista->arrayDinamico = malloc(sizeof(DADO));
+  if (lista == NULL && lista->arrayDinamico == NULL)
+  {
+    perror("Erro na criação da lista");
+    exit(EXIT_FAILURE);
+  }
+  lista->tam = 0;
+  return lista;
+}
+
+int removeNprimeirosItensListaDinamica(ListaDinamica *lista, u_long numeroDeItensParaEliminar)
+{
+  return removeNPrimeirosIntensLista(lista->arrayDinamico, sizeof(DADO), numeroDeItensParaEliminar, &lista->tam);
+}
+
+u_long getTamListaDinamica(ListaDinamica lista)
+{
+  return lista.tam;
+}
+
+void appendListaDinamica(ListaDinamica *lista, DADO dadoToAppend)
+{
+  lista->arrayDinamico = realloc(lista->arrayDinamico, (lista->tam + 1) * sizeof(DADO));
+  if (lista->arrayDinamico == NULL)
+  {
+    perror("Erro na inserção de elemento");
+    exit(EXIT_FAILURE);
+  }
+  lista->arrayDinamico[lista->tam++] = dadoToAppend;
+}
+
+/*
+void exibeTeste(ListaDinamica *listinha)
+{
+  char strToPrint[] = "%c, ";
+  for (size_t i = 0; i < listinha->tam; i++)
+  {
+    printf(strToPrint, listinha->arrayDinamico[i].valor);
+  }
+  putchar('\n');
 }
 
 int main(int argc, char const *argv[])
 {
-  ListaDinamica *listinha = malloc(sizeof(ListaDinamica));
-  listinha->arrayDinamico = malloc(5 * sizeof(DADO));
-  // listinha->arrayDinamico[0].valor = 0;
-  listinha->arrayDinamico[4].valor = 'c';
-
-
-  char strToPrint[] = "%c, ";
-  for (size_t i = 0; i < 5; i++)
+  ListaDinamica *listinha = createListaDinamica();
+  for (char i = 'a'; i < 'a' + 10; i++)
   {
-    printf(strToPrint, listinha->arrayDinamico[i].valor);
+    DADO a = {i};
+    appendListaDinamica(listinha, a);
   }
-  putchar('\n');
-  removeNPrimeirosIntensLista(listinha->arrayDinamico, sizeof(listinha->arrayDinamico->valor), 4, 5);
-  for (size_t i = 0; i < 5; i++)
-  {
-    printf(strToPrint, listinha->arrayDinamico[i].valor);
-  }
-  putchar('\n');
+  exibeTeste(listinha);
+  removeNPrimeirosIntensLista(listinha->arrayDinamico, sizeof(DADO), 9, &listinha->tam) ? puts("true") : puts("false");
+  exibeTeste(listinha);
   return 0;
 }
+*/
