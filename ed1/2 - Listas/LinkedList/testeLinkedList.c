cd531be6306209a08c44eff785754ad625fe10d1
ellian_aragao <ellian.aragao@gmail.com>
2020-08-27 14:24:33 -0300

    adequação do teste para refinar o código da biblioteca

--- ed1/2 - Listas/LinkedList/testeLinkedList.c
+++ ed1/2 - Listas/LinkedList/testeLinkedList.c
@@ -4,12 +4,12 @@
 
 void printItem(void *item)
 {
-  printf("%f ,", *(float *)item);
+  printf("%d ,", *(int *)item);
 }
 
 int compareItens(void *item1, void *item2)
 {
-  if (*(float*)item1 == *(float*)item2)
+  if (*(int *)item1 == *(int *)item2)
     return 1;
   return 0;
 }
@@ -17,18 +17,22 @@ int compareItens(void *item1, void *item2)
 int main()
 {
   LinkedList *list = createLinkedList();
-  float *item = malloc(sizeof(float));
-  *item = 2.7182;
-  appendLinkedList(list, item);
-  float *item2 = malloc(sizeof(float));
-  *item2 = 3.141592;
-  appendLinkedList(list, item2);
-  forEach(list, printItem);
+  int *item;
+  for (int i = 0; i < 5; i++)
+  {
+    item = malloc(sizeof(int));
+    *item = i;
+    appendLinkedList(list, item);
+  }
+
+  forEachItem(list, printItem);
   putchar('\n');
+
   removeLinkedList(list, item, compareItens);
-  removeLinkedList(list, item, compareItens);
-  forEach(list, printItem);
+
+  forEachItem(list, printItem);
   putchar('\n');
+
   destroyLinkedList(list);
   return 0;
 }
