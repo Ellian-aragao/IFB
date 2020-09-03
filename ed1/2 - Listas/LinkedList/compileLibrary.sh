8581282c4803c05d4d8b1572623f496dc0234f0d
ellian_aragao <ellian.aragao@gmail.com>
2020-09-03 12:29:08 -0300

    ajuste para teste

--- ed1/2 - Listas/LinkedList/compileLibrary.sh
+++ ed1/2 - Listas/LinkedList/compileLibrary.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd '/home/ellian/code/faculdade/ed1/2 - Listas/LinkedList/'
-gcc -g -c linkedList.c testInterface/testLinkedListInterface.c
+gcc -g linkedList.c testInterface/testLinkedListInterface.c testeLinkedList.c
 
-mv 'linkedList.o' '/home/ellian/code/faculdade/ed1/2 - Listas/6/encadeada/'
-mv 'testLinkedListInterface.o' '/home/ellian/code/faculdade/ed1/2 - Listas/6/encadeada/'
-cd '/home/ellian/code/faculdade/ed1/2 - Listas/6/encadeada'
+# mv 'linkedList.o' '/home/ellian/code/faculdade/ed1/2 - Listas/6/encadeada/'
+# mv 'testLinkedListInterface.o' '/home/ellian/code/faculdade/ed1/2 - Listas/6/encadeada/'
+# cd '/home/ellian/code/faculdade/ed1/2 - Listas/6/encadeada'
 
-gcc -g concatenaLinkedList.c testaConcatenaLinkedList.c linkedList.o testLinkedListInterface.o
+# gcc -g concatenaLinkedList.c testaConcatenaLinkedList.c linkedList.o testLinkedListInterface.o
