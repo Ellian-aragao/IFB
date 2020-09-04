aaa06bb8b7964b0a5cc27c04c403daf1e1afdddf
ellian_aragao <ellian.aragao@gmail.com>
2020-09-04 01:12:59 -0300

    ajuste para compilação

--- ed1/2 - Listas/LinkedList/compileLibrary.sh
+++ ed1/2 - Listas/LinkedList/compileLibrary.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 cd '/home/ellian/code/faculdade/ed1/2 - Listas/LinkedList/'
-# gcc -g linkedList.c testInterface/testLinkedListInterface.c testeLinkedList.c
-gcc -c -g linkedList.c testInterface/testLinkedListInterface.c
+gcc -g linkedList.c testInterface/testLinkedListInterface.c testeLinkedList.c
+# gcc -c -g linkedList.c testInterface/testLinkedListInterface.c
 
-pathTest=`/home/ellian/code/faculdade/ed1/2 - Listas/7/encadeada`
+# pathTest=`/home/ellian/code/faculdade/ed1/2 - Listas/7/encadeada`
 
 # mv 'linkedList.o' `$pathTest`
 # mv 'testLinkedListInterface.o' `$pathTest`
-echo "var $pathTest"
-cd `$pathTest`
-pwd
+
+
 # gcc -g concatenaLinkedList.c testaConcatenaLinkedList.c linkedList.o testLinkedListInterface.o
