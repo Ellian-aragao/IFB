704f0cab05b5680925c738c9212b50aa0d33f571
ellian_aragao <ellian.aragao@gmail.com>
2020-09-08 21:45:57 -0300

    renomeando pastas para correção de bug

---
+++ ed1/2_Listas/LinkedList/compileLibrary.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pathListas=~/code/faculdade/ed1/2_Listas/
+cd $pathListas
+pwd
+# gcc -g linkedList.c testInterface/testLinkedListInterface.c testeLinkedList.c
+# gcc -c -g linkedList.c testInterface/testLinkedListInterface.c
+
+# dest=../15
+
+# mv linkedList.o $dest
+# mv testLinkedListInterface.o $dest
+# cd $dest
+
+# # gcc -g linkedList.o testLinkedListInterface.o verificaSeEstaOrdenado.c testeverificaSeEstaOrdenado.c
+# rm linkedList.o testLinkedListInterface.o
+# # ./a.out
