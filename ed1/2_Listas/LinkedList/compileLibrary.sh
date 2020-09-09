3d60b80953cab64fccf14f0de114bd8f767d1be8
ellian_aragao <ellian.aragao@gmail.com>
2020-09-08 23:40:16 -0300

    shellScript finalizado

--- ed1/2_Listas/LinkedList/compileLibrary.sh
+++ ed1/2_Listas/LinkedList/compileLibrary.sh
@@ -1,17 +1,50 @@
 #!/bin/bash
 
-pathListas=~/code/faculdade/ed1/2_Listas/
-cd $pathListas
-pwd
-# gcc -g linkedList.c testInterface/testLinkedListInterface.c testeLinkedList.c
-# gcc -c -g linkedList.c testInterface/testLinkedListInterface.c
+#faz a compilação da biblioteca
+compile_library() {
+  echo 'Compilando biblioteca LinkedList'
+  gcc -c -g linkedList.c testInterface/testLinkedListInterface.c
+}
 
-# dest=../15
+# gera binário final utilizando o teste do exercício e a interface dele com a biblioteca
+compile_exercicio() {
+  echo 'Compilando binário final'
+  gcc -g $(ls *.c *.o)
+  rm $(ls *.o)
+}
 
-# mv linkedList.o $dest
-# mv testLinkedListInterface.o $dest
-# cd $dest
+# move os arquivos da biblioteca para pasta destino
+move_library() {
+  dest=$1
+  mv linkedList.o $dest
+  mv testLinkedListInterface.o $dest
+  cd $dest
+}
 
-# # gcc -g linkedList.o testLinkedListInterface.o verificaSeEstaOrdenado.c testeverificaSeEstaOrdenado.c
-# rm linkedList.o testLinkedListInterface.o
-# # ./a.out
+# executa binário padrão gerado pelo compilador gcc
+execute_binary() {
+  echo -e 'Executando exercício\n--------------------\n'
+  ./a.out
+}
+
+# faz a compilação conforme parâmetro enviado como argumento
+fluxo_execucao() {
+  if [ -z $1 ]; then
+    echo 'não foi enviado path'
+    exit
+  elif [ "$1" != "LinkedList" ] && [ "$1" != "LinkedList/" ]; then
+    compile_library
+    move_library $pathLista$1
+  else
+    gcc -c -g testInterface/testLinkedListInterface.c
+  fi
+  compile_exercicio
+  execute_binary
+}
+
+pathLista=~/code/faculdade/ed1/2_Listas/
+path=$1
+
+cd $pathLista/LinkedList
+fluxo_execucao $path
+rm a.out
