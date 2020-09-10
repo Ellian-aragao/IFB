69e5e4dc38a82e8e3ec0c2b6e81ff5331f4d60e0
Ellian Aragão Dias <ellian.aragao@gmail.com>
2020-09-10 20:59:16 -0300

    versão que funcionou

--- ed1/2_Listas/20/divideLinkedLists.c
+++ ed1/2_Listas/20/divideLinkedLists.c
@@ -1,14 +1,31 @@
 #include "divideLinkedLists.h"
 
-LinkedList *divideLinkedListByN(LinkedList *list, u_long n)
+LinkedList *global;
+u_long indexNumber = 0;
+u_long n_da_funcao;
+
+void saveAtIndex(void *item)
+{
+  if (n_da_funcao < indexNumber)
+  {
+    appendLinkedList(global, item);
+  }
+  indexNumber++;
+}
+
+LinkedList *divideLinkedListByN(LinkedList *list, const u_long n)
 {
   if (list)
   {
+    n_da_funcao = n;
     LinkedList *list2 = createLinkedList(getSizeofItensLinkedList(list));
+    global = list2;
+    forEachLinkedList(list, saveAtIndex);
+
     for (u_long i = getTamLinkedList(list); i > n; i--)
     {
-      void* addr = getLastItemLinkedList(list);
-      appendLinkedList(list2, addr);
+      // void* addr = getLastItemLinkedList(list);
+      // appendLinkedList(list2, addr);
       removeLastItemLinkedList(list);
     }
     return list2;
