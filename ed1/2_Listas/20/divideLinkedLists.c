e1581ddb874303a7ad2b0085ef3ac9e93931d7c1
Ellian Aragão Dias <ellian.aragao@gmail.com>
2020-09-10 22:10:17 -0300

    versão antes de buscar deixar enxuto

--- ed1/2_Listas/20/divideLinkedLists.c
+++ ed1/2_Listas/20/divideLinkedLists.c
@@ -21,12 +21,27 @@ LinkedList *divideLinkedListByN(LinkedList *list, const u_long n)
     LinkedList *list2 = createLinkedList(getSizeofItensLinkedList(list));
     global = list2;
     forEachLinkedList(list, saveAtIndex);
+    for (u_long i = getTamLinkedList(list); i > n; i--)
+      removeLastItemLinkedList(list);
+    return list2;
+  }
+  return NULL;
+}
 
+LinkedList *divideLinkedListByNProblematic(LinkedList *list, u_long n)
+{
+  if (list)
+  {
+    LinkedList *list2 = createLinkedList(getSizeofItensLinkedList(list));
     for (u_long i = getTamLinkedList(list); i > n; i--)
     {
-      // void* addr = getLastItemLinkedList(list);
-      // appendLinkedList(list2, addr);
+      size_t tamItem = getSizeofItensLinkedList(list);
+      void *dest = malloc(tamItem);
+      void *src = getLastItemLinkedList(list);
+      memcpy(dest, src, tamItem);
+      appendLinkedList(list2, dest);
       removeLastItemLinkedList(list);
+      free(dest);
     }
     return list2;
   }
