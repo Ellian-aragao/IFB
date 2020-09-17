fc9161f6b00cd270c1b3b7b71e44f64a3cc65ad9
Ellian Aragão Dias <ellian.aragao@gmail.com>
2020-09-17 00:07:16 -0300

    ajustando para melhor leitura e reduzindo tempo caso tamanho da string seja diferente

--- ed1/3_Pilha/5/stringXeReversaDeY.c
+++ ed1/3_Pilha/5/stringXeReversaDeY.c
@@ -3,11 +3,13 @@
 bool stringXeReversaDeY(char *x, const char *y)
 {
   const size_t strLength = strlen(x);
-  
+  if (strLength != strlen(y))
+    return false;
+
   Stack *stackStr = createStack(sizeof(char));
   for (size_t i = 0; i < strLength; i++)
     pushStack(stackStr, &x[i]);
-  
+
   char inversaStr[strLength];
   for (size_t i = 0; i < strLength; i++)
   {
@@ -15,6 +17,6 @@ bool stringXeReversaDeY(char *x, const char *y)
     popStack(stackStr);
   }
   destroyStack(stackStr);
-  
+
   return strncmp(inversaStr, y, strLength) == 0 ? true : false;
 }
