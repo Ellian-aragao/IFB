3cbd941f818f50925032098f1c98037a1d5b1589
Ellian Aragão Dias <ellian.aragao@gmail.com>
2020-09-18 19:17:31 -0300

    removendo main

--- ed1/3_Pilha/12/compareTamStack.c
+++ ed1/3_Pilha/12/compareTamStack.c
@@ -6,17 +6,3 @@ int compareTamStack(Stack *s1, Stack *s2)
          s2Tam = getTamStack(s2);
   return s1Tam < s2Tam ? -1 : s1Tam > s2Tam ? 1 : 0;
 }
-
-int main()
-{
-  Stack* s1 = createStack(sizeof(char));
-  Stack* s2 = createStack(sizeof(char));
-  char a = 'a';
-  pushStack(s1, &a);
-  pushStack(s2, &a);
-  pushStack(s1, &a);
-  printf("%d\n",compareTamStack(s1,s2));
-  destroyLinkedList(s1);
-  destroyLinkedList(s2);
-  return 0;
-}
