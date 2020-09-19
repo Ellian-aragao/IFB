e808f0708b0e520c4190e9d6797e22f8d0c7bb5f
Ellian Aragão Dias <ellian.aragao@gmail.com>
2020-09-18 21:38:54 -0300

    deixando código mais enxudo e usando lógica da pilha

--- ed1/3_Pilha/8/analizadorDeExpressao.c
+++ ed1/3_Pilha/8/analizadorDeExpressao.c
@@ -4,39 +4,26 @@ bool analizadorDeExpressao(char *str)
 {
   Stack *s = createStack(sizeof(char));
   char *ptr_str = str;
+  bool var = true;
+
   while (*ptr_str)
   {
-      // printf("%d\n",*ptr_str);
-    if (*ptr_str == '(' || ')' == *ptr_str){
+    if (*ptr_str == '(')
       pushStack(s, ptr_str);
+    if (')' == *ptr_str)
+    {
+      if (getTamStack(s))
+        popStack(s);
+      else
+      {
+        var = false;
+        break;
+      }
     }
     ptr_str++;
   }
-  u_long tamStack = getTamStack(s);
-  int cont = 0;
-  while (tamStack)
-  {
-    char *var = getPopStack(s);
-    printf("'%c' %ld\n", *var, tamStack);
-    if (*var == '(')
-      cont++;
-    else if (*var == ')')
-      cont--;
-    else {
-      cont = 1;
-      break;
-    }
 
-    free(var);
-    tamStack--;
-  }
+  u_long tam = getTamStack(s);
   destroyStack(s);
-  return !cont;
-}
-
-int main()
-{
-  char str[] = "";
-  puts(analizadorDeExpressao(str) ? "Correto" : "Incorreto");
-  return 0;
+  return var && !tam;
 }
