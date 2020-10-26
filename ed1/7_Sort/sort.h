61c5fe29b57d8d3502701056de7d413a394b649b
Ellian Aragão Dias <ellian.aragao@gmail.com>
2020-10-26 19:34:42 -0300

    exibição da biblioteca header

--- ed1/7_Sort/sort.h
+++ ed1/7_Sort/sort.h
@@ -6,5 +6,7 @@
 void bubbleSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
 void insertionSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
 void insertionSortItem(void *vector, u_long tamVector, void *item, size_t SizeValuesVector, int (*comparator)(void *, void *));
+void quickSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
+void selectionSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
 
 #endif
