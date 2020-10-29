c37b0c523aa46cdbf5908aa318ea772e49f8960c
Ellian Aragão Dias <ellian.aragao@gmail.com>
2020-10-29 01:30:37 -0300

    habilitando outros algoritmos de sort para usuário

--- ed1/7_Sort/sort.h
+++ ed1/7_Sort/sort.h
@@ -8,5 +8,8 @@ void insertionSort(void *vector, u_long tamVector, size_t SizeValuesVector, int 
 void insertionSortItem(void *vector, u_long tamVector, void *item, size_t SizeValuesVector, int (*comparator)(void *, void *));
 void quickSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
 void selectionSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
+void shellSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
+void mergeSort(void *vector, const u_long tamVector, const size_t SizeValuesVector, int (*comparator)(void *, void *));
+void heapSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
 
 #endif
