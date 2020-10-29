decfc7e6183790d23a3c1018afd8bd89f9d9faa7
Ellian Aragão Dias <ellian.aragao@gmail.com>
2020-10-29 12:33:33 -0300

    reclarando interface comum a todas as funções de sort

--- ed1/7_Sort/sort.h
+++ ed1/7_Sort/sort.h
@@ -3,13 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-void bubbleSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
-void insertionSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
+#define SORT_FUNCS_ARGUMENTS void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *)
+
+void bubbleSort(SORT_FUNCS_ARGUMENTS);
+void insertionSort(SORT_FUNCS_ARGUMENTS);
 void insertionSortItem(void *vector, u_long tamVector, void *item, size_t SizeValuesVector, int (*comparator)(void *, void *));
-void quickSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
-void selectionSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
-void shellSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
-void mergeSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
-void heapSort(void *vector, u_long tamVector, size_t SizeValuesVector, int (*comparator)(void *, void *));
+void quickSort(SORT_FUNCS_ARGUMENTS);
+void selectionSort(SORT_FUNCS_ARGUMENTS);
+void shellSort(SORT_FUNCS_ARGUMENTS);
+void mergeSort(SORT_FUNCS_ARGUMENTS);
+void heapSort(SORT_FUNCS_ARGUMENTS);
 
 #endif
