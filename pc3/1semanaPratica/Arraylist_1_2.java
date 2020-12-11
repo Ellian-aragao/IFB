073b58ec63dd5b1d7a81d9eb9b3038cc5c2c3bd9
Ellian Aragão <ellian.aragao@gmail.com>
2020-12-11 01:02:14 -0300

    inserindo atividades para enviar ao professor

---
+++ pc3/1semanaPratica/Arraylist_1_2.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arraylist_1_2 {
+  public static void main(String[] args) {
+    List<String> array = exercicio1();
+    exercicio2(array);
+  }
+  
+  public static List<String> exercicio1() {
+    List<String> array = new ArrayList<>();
+    array.add("vermelho");
+    array.add("verde");
+    array.add("azul");
+    array.add("marrom");
+    array.add("amarelo");
+    array.forEach((cor) -> System.out.println(cor));
+    return array;
+  }
+
+  public static void exercicio2(List<String> array) {
+    array.add(0, "roxo");
+    array.remove(2);
+    List<String> cpArray = new ArrayList(array);
+    // TODO sort lista
+  }
+}
